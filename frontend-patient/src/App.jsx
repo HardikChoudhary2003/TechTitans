7b2ec624bfb1d6a68b85e0648eb1e0b08bc20d76
Hardik <hardikchoudhary703@gmail.com>
2025-12-04 17:28:49 +0530

    [Frontend: Patient] Build Patient Login Page /patient/login

---
+++ frontend-patient/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/patient/login" element={<LoginPage />} />
+            <Route path="/patient/dashboard" element={<h1>Dashboard Placeholder</h1>} />
+            <Route path="*" element={<Navigate to="/patient/login" replace />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
